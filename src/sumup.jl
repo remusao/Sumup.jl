8a22a6163d6d79e45a301dffcddf014c8de3e2ff
Rémi Berson <remi.berson@gmail.com>
2013-11-12 12:37:24 +0100

    improved preproc, improved results

--- src/sumup.jl
+++ src/sumup.jl
@@ -1,4 +1,27 @@
 
+function getsentences(corpus::Corpus, preproc!::Function)
+
+    docs = map(documents(corpus)) do x
+        x |> text |> d -> split(d, r"[.!?]+")
+    end
+
+    sentences = {}
+    for d in docs
+        for s in d
+            stringdoc = StringDocument(s)
+            preproc!(stringdoc)
+            preprocs = text(stringdoc)
+            tokens = TextAnalysis.tokenize(TextAnalysis.EnglishLanguage, preprocs)
+            if (length(tokens)) > 1
+                push!(sentences, (strip(s, [' ', '\n']), tokens))
+            end
+        end
+    end
+
+    return sentences
+end
+
+
 function sumup(path::String, top::Integer, k::Integer, topwords::Integer)
 
     # Create corpus
@@ -6,15 +29,29 @@ function sumup(path::String, top::Integer, k::Integer, topwords::Integer)
     standardize!(corpus, StringDocument)
 
     # Preproc
-    remove_corrupt_utf8!(corpus)
+    const preprocflag = ( strip_case
+                        | strip_punctuation
+                        | strip_corrupt_utf8
+                        | strip_articles
+                        | strip_prepositions
+                        | strip_pronouns
+                        | strip_stopwords
+                        | strip_articles)
+    preproc!(doc) =  prepare!(doc, preprocflag)
 
+    # Extract sentences
+    sentences = getsentences(corpus, preproc!)
 
+    # Apply preproc on corpus
+    preproc!(corpus)
+
+    # Update corpus internals
     update_lexicon!(corpus)
     update_inverse_index!(corpus)
 
     # Topic Extraction
     m = DocumentTermMatrix(corpus)
-    model = nmf(m, k)
+    model = lda(m, k)
     topics = getTopics(model, m.terms, k, topwords)
 
 
@@ -22,61 +59,34 @@ function sumup(path::String, top::Integer, k::Integer, topwords::Integer)
     # Sumup topics #
     # ------------ #
 
-
-    # Get sentences in documents
-    docs = map(documents(corpus)) do x
-        x |> text |> document -> split(document, r"[.:!?()]+")
-    end
-
     # Argsort function
     argsort(x) = sort([1: length(x)], by = (i -> x[i]), rev = true)
 
-    # Get sentences
-    sentences = {}
-    for d in docs
-        for s in d
-            if length(s) != 0
-                tokens = TextAnalysis.tokenize(TextAnalysis.EnglishLanguage, s)
-                if (length(tokens)) > 5
-                    map!(t -> strip(t, ['\'', '"', ',', '.', ':', '!', '?', '(', ')']), tokens)
-                    push!(sentences, tokens)
-                end
-            end
-        end
-    end
-    println("Number of sentences: $(length(sentences))")
-
-    # Extract sumup of each topic
-    println("Extracting summaries")
-
     for topic in topics
 
-        # Strip punctuation
-        words = map(t -> strip(t, ['\'', '"', ',', '.', ':', '!', '?', '(', ')']), topic.words)
-        proba = Dict{UTF8String, Float64}(words, topic.coeffs)
+        # Dict: word => proba
+        proba = Dict{UTF8String, Float64}(topic.words, topic.coeffs)
 
         # Weight of each sentence
         topsentences = zeros(Float64, length(sentences))
 
         # For each sentence
         for (i, s) in enumerate(sentences)
+
             # For each token in sentence
-            wcount = 1
-            for tok in s
-                if haskey(proba, tok)
-                    wcount += 1
-                end
+            for tok in s[2]
                 # Increase word weight with its probability
                 topsentences[i] += get(proba, tok, 0.0)
             end
+
             # Normalize weight
-            topsentences[i] /= wcount
+            topsentences[i] /= length(s[2])
         end
 
+        println("\nTopic")
         # Print top sentences for current topic
-        println("Topic")
         for i in argsort(topsentences)[1:top]
-            println(join(sentences[i], " "))
+            println(sentences[i][1])
         end
     end
 end
